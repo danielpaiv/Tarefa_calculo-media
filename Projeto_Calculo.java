4e235cc9e60590af66caa6779508f5ec86d65a72
unknown <dpaiva266@hotmail.com>
2024-02-19 14:25:59 -0300

    *Calculo executando uma soma de quatro valores e dividindo por 4

---
+++ Projeto_Calculo.java
@@ -0,0 +1,26 @@
+public class Projeto_Calculo {
+    public static void main(String args[]){
+
+        operacoesaritimetricas();
+    }
+
+    private static void operacoesaritimetricas() {
+
+        int nun1 = 20;
+        int nun2 =50;
+        int nun3 = 10;
+        int nun4 =40;
+
+        int nun5 = nun1 + nun2;
+        int nun6 = nun5 + nun3;
+        int nun7 = nun6 + nun4;
+
+        int nun8 = nun7 /4;
+
+        System.out.println(nun8);
+
+
+    }
+
+
+}
